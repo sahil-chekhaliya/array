b31b3e915667f91d2d42c69df8f14e5623558691
sahil-chekhaliya <sahilchekhaliya65@gmail.com>
2024-05-07 13:04:20 +0530

    add new file & code

---
+++ swapalternative.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int arr[],int n)
+{
+
+int temp;
+
+for (int i=0;i<n-1;i+=2)
+
+{// you can use inbuilt swap function
+          
+         // or
+
+    // using third variable approach
+    temp=arr[i];
+    arr[i]=arr[i+1];
+    arr[i+1]=temp;
+
+    // without third variable first approach
+    /*
+       arr[i]=arr[i]+arr[i+1];
+       arr[i+1]=arr[i]-arr[i+1];
+       arr[i]=arr[i]-arr[i+1];
+    */
+
+   // without using third variable second approach
+   /*
+   arr[i]=arr[i]*arr[i+1];
+   arr[i+1]=arr[i]/arr[i+1];
+   arr[i]=arr[i]/arr[i+1];
+  */
+}
+}
+
+void printarray(int a[],int n)
+{
+
+for (int i=0;i<n;i++)
+{
+
+    cout<<a[i]<<" ";
+
+}
+
+cout<<endl;
+    
+}
+
+int main()
+{
+
+
+    int arr[10]={71,42,46,7,18};
+
+cout<<"original array"<<endl;
+    printarray(arr,5);
+    swap(arr,5);
+
+
+cout<<"after swapping array:"<<endl;
+     printarray(arr,5);
+
+}
